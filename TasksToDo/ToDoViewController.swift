c479179f184b24245fa5a4a2766268ef1b910071
Jason Whittaker <jason_bournemouth@yahoo.co.uk>
2018-02-18 15:56:39 +0700

    TableView delegates for Selectog and deselecting a task

--- TasksToDo/ToDoViewController.swift
+++ TasksToDo/ToDoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
     
-    let itemArray = ["Buy Headphones", "Buy Coffee", "Learn Udemy"]
+    var itemArray = ["Buy Headphones", "Buy Coffee", "Learn Udemy"]
 
 
     override func viewDidLoad() {
@@ -53,6 +53,31 @@ class ToDoViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
         print(indexPath.row)
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New To Do Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+        self.itemArray.append(textField.text!)
+            
+        self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
 
 }
